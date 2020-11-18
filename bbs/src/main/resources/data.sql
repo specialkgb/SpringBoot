d70eb244f52609c2e41a80a77ef64f12b8ca3152
specialkgb <specialkgb@naver.com>
2020-11-18 16:34:50 +0900

    Hell_Boot

---
+++ bbs/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into tbl_bbs(b_writer, b_date_time, b_content, b_count)
+values('홍길동', '2020-11-18','게시판 만들기', 0);
+
+insert into tbl_bbs(b_writer, b_date_time, b_content, b_count)
+values('이몽룡', '2020-11-18','게시판 만들기', 0);
+
+insert into tbl_bbs(b_writer, b_date_time, b_content, b_count)
+values('성춘향', '2020-11-18','게시판 만들기', 0);
